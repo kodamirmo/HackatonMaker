d595503d95a25777eece84de5fa337f2e26b40f1
kodamirmo <irvingemmanuelgg@gmail.com>
2014-12-15 16:49:43 -0600

    Add JS

---
+++ Game/client/views/game/game.js
@@ -0,0 +1,7 @@
+Template.game.rendered = function(){
+
+	//Aqui se hace todo lo que se tenga que hacer cuando se acabe de cargar el DOM
+	//Puedes usar jquery directo aqui sin hacer nada mas que escribirlo
+
+	
+};
