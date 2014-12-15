709c5d020aae448cc956d05665065faf83f5b06b
kodamirmo <irvingemmanuelgg@gmail.com>
2014-12-15 16:56:50 -0600

    Update game

--- Game/client/views/game/game.js
+++ Game/client/views/game/game.js
@@ -3,5 +3,17 @@ Template.game.rendered = function(){
 	//Aqui se hace todo lo que se tenga que hacer cuando se acabe de cargar el DOM
 	//Puedes usar jquery directo aqui sin hacer nada mas que escribirlo
 
-	
+
 };
+
+
+Template.game.events({
+	
+	'click .tecla':function(event){
+		event.preventDefault();
+    	
+    	console.log( $(this).attr('id') )
+
+	}
+
+});
