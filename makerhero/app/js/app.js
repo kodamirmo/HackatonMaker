e04ffebf5b971667bb57c67134a7778adf0d5cc1
Kevin Martinez <n4utilius@gmail.com>
2014-12-17 08:38:32 -0600

    Primera iteracion de la logica del juego

---
+++ makerhero/app/js/app.js
@@ -0,0 +1,45 @@
+$(function() {  
+
+	var socket = io.connect('http://localhost:4000');
+	var my_sequence = [];
+
+	$(".tecla").on("click", function(){
+		var num = parseInt( $(this).attr("id").split("_")[1] );
+		my_sequence.push(num);
+	})
+
+	$("#start").on("click", function(){
+		socket.emit('start', true );
+		return false
+	})
+
+	socket.on('you turn', function(data){
+		var time_out = 10;
+
+		console.log("---- secuencia otiginal ----")
+		console.log(data.data)
+		
+
+		console.log("---- start!!! ----")
+
+		var myVar = setInterval( function(){ 
+			console.log(time_out);
+			time_out--;
+			if(time_out == 0){
+				clearInterval(myVar)
+				socket.emit('send my_sequence', my_sequence );
+			} 
+		}, 1000);
+	});
+
+	socket.on('result', function(msg){
+		console.log("---- secuencia del usuario ----")
+		console.log(my_sequence)
+		
+		console.log("---- secuencia del usuario ----")
+		console.log(msg)
+	})
+
+
+});  
+  
---
+++ server/app.js
@@ -0,0 +1,47 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var socket = require('socket.io')(http);
+
+http.listen(4000, function(){
+  console.log('listening on *:4000');
+});
+
+
+socket.on('connection', function(socket){
+  	socket.on('start', function(msg){
+  		if(msg)	start_game(animate_board)		
+  	});
+
+  	socket.on('send my_sequence', function(my_sequence){
+  		console.log(my_sequence, the_sequense)
+  		if(my_sequence === the_sequense)
+  			socket.emit('result', { "status": "winner" });
+  		else
+  			socket.emit('result', { "status": "looser" });
+  	});	
+});
+
+var animate_board = function(the_sequense, callback){
+	//codigo para animar la placa
+	callback(the_sequense)
+}
+
+var start_game = function(callback){
+	the_sequense = generate_sequence(1)
+	callback(the_sequense, you_turn)
+}
+
+var you_turn = function(the_sequense){
+	socket.emit('you turn', { "data": the_sequense });	
+}
+
+var generate_sequence = function(num){
+	var MIN = 0,  
+		MAX = 15,
+		sequence = [];
+	for (var i = 1; i <= num; i++){
+		sequence.push( Math.floor(Math.random() * (MAX - MIN + 1)) + MIN );
+	}
+	console
+	return sequence;
+}
